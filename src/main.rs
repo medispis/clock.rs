4ad7bdc72c1e5fb297e9be617e3904e15091388f
medispis <medispis@gmail.com>
2025-12-30 17:17:24 +0100

    clock is working (just text)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 extern crate chrono;
-use std::thread;
-
+use crate::event::Event;
 use chrono::prelude::*;
+use crossterm::event::{self, KeyCode};
+use std::time::Duration;
 
-fn main() {
-    loop {
-        let utc: DateTime<Utc> = Utc::now();
-        thread::sleep(std::time::Duration::from_millis(1000));
-        let clock = utc.format("%H:%M:%S");
-        println!("{}", clock);
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ratatui::run(|terminal| {
+        loop {
+            let clock = get_time();
+            terminal.draw(|frame| frame.render_widget(clock, frame.area()))?;
+
+            // Check for key events with a short timeout
+            if event::poll(Duration::from_millis(100))? {
+                if let Event::Key(key) = event::read()? {
+                    if key.code == KeyCode::Char('q') || key.code == KeyCode::Char('c') {
+                        break;
+                    }
+                }
+            }
+        }
+        Ok(())
+    })
+}
+fn get_time() -> String {
+    let utc: DateTime<Utc> = Utc::now();
+    utc.format("%H:%M:%S").to_string()
 }
